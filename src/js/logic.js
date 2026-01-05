b93e316a19810ca5b4e2f8c0c4a887d128e94793
Elina test <elina.ulli93@gmail.com>
2026-01-05 14:00:53 +0500

    Fix problem

--- src/js/logic.js
+++ src/js/logic.js
@@ -1,52 +1,54 @@
 export default class Logic {
-  constructor(gui) {
-    this.gui = gui;
-    this.togglePopover = this.togglePopover.bind(this);
-    this.hidePopover = this.hidePopover.bind(this);
-  }
+	constructor(gui) {
+		this.gui = gui;
+		this.togglePopover = this.togglePopover.bind(this);
+		this.hidePopover = this.hidePopover.bind(this);
+	}
 
-  init() {
-    this.gui.drawUi();
-    this.gui.button.addEventListener("click", this.togglePopover);
-    document.addEventListener("click", this.hidePopover);
-  }
+	init() {
+		this.gui.drawUi();
+		this.gui.button.addEventListener("click", this.togglePopover);
+		document.addEventListener("click", this.hidePopover);
+	}
 
-  hidePopover(e) {
-    if (e.target !== this.gui.button && !this.gui.popover.contains(e.target) && !this.gui.popover.classList.contains("hidden")) {
-      popover.classList.remove("show");
-      popover.classList.add("hiding");
-    }
-    }
+	hidePopover(e) {
+		if (
+			e.target !== this.gui.button &&
+			!this.gui.popover.contains(e.target) &&
+			!this.gui.popover.classList.contains("hidden")
+		) {
+			this.gui.popover.classList.remove("show");
+			this.gui.popover.classList.add("hiding");
+		}
+	}
 
+	togglePopover(e) {
+		e.preventDefault();
+		e.stopPropagation();
+		const position = e.target.getBoundingClientRect();
+		const popover = this.gui.popover;
 
-  togglePopover(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    const position = e.target.getBoundingClientRect();
-    const popover = this.gui.popover;
+		if (popover.classList.contains("hidden")) {
+			popover.classList.remove("hidden");
 
-    if (popover.classList.contains("hidden")) {
-      popover.classList.remove("hidden");
+			popover.style.position = "absolute";
+			popover.style.top = `${position.bottom + window.scrollY}px`;
+			popover.style.left = `${position.left + window.scrollX}px`;
+			popover.style.marginTop = "5px";
 
-      popover.style.position = 'absolute';
-      popover.style.top = `${position.bottom + window.scrollY}px`;
-      popover.style.left = `${position.left + window.scrollX}px`;
-      popover.style.marginTop = '5px';
-
-      popover.classList.remove("hiding");
-      requestAnimationFrame(()=> {
-        requestAnimationFrame(()=> {
-          popover.classList.add("show");
-        });
-      });
-      
-    } else {
-      popover.classList.remove('show');
-      popover.classList.add("hiding");
-      setTimeout(()=> {
-        popover.classList.add("hidden");
-        popover.classList.remove("hiding");
-      }, 300)
-    }
-  }
+			popover.classList.remove("hiding");
+			requestAnimationFrame(() => {
+				requestAnimationFrame(() => {
+					popover.classList.add("show");
+				});
+			});
+		} else {
+			popover.classList.remove("show");
+			popover.classList.add("hiding");
+			setTimeout(() => {
+				popover.classList.add("hidden");
+				popover.classList.remove("hiding");
+			}, 300);
+		}
+	}
 }
